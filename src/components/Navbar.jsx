ef1cc22d1e97bcb0a9d120623317ad46b57a9f4b
daniels9028 <daniels9028@student.untan.ac.id>
2024-06-26 10:46:23 +0700

    feat: navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { LuAlignRight } from "react-icons/lu";
 import { IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -24,13 +25,31 @@ const Navbar = () => {
           Download App
         </button>
 
-        <div className="lg:hidden flex" onClick={() => setOpen((o) => !o)}>
+        <div
+          className="lg:hidden text-gray-800 flex z-[70]"
+          onClick={() => setOpen((o) => !o)}
+        >
           {open ? <IoMdClose size={30} /> : <LuAlignRight size={30} />}
         </div>
 
         {open && (
-          <div className="absolute lg:hidden flex flex-col lg:top-14 top-[70px] right-10 w-[300px] rounded-xl items-center text-center bg-[#ebe9e7] z-20">
-            <ul className="my-2 space-y-2 text-[#006041] font-semibold text-[16px] leading-[24px] capitalize not-italic cursor-pointer">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              visible: {
+                opacity: 1,
+                x: 0,
+                transition: {
+                  duration: 0.4,
+                },
+              },
+              hidden: { opacity: 0, x: "100%" },
+            }}
+            className="absolute lg:hidden flex flex-col top-0 right-0 w-full min-h-screen gap-5 justify-center items-center text-center bg-[#ebe9e7] z-50"
+          >
+            <img src={logo} alt="" />
+            <ul className="my-2 space-y-6 text-[#006041] font-bold text-[16px] leading-[24px] capitalize not-italic cursor-pointer">
               <li>Tentang</li>
               <li>Menu</li>
               <li>Kolaborasi</li>
@@ -42,7 +61,7 @@ const Navbar = () => {
             <button className="py-2 px-6 border-[1.5px] text-black border-black rounded-full hover:text-white hover:bg-[#006041] transition-colors my-4 w-[200px]">
               Download App
             </button>
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
